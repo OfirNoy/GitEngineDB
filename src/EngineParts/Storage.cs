874c5d178146a5be9caae8a49b541c6ea6ebae78
Ofir Noy <ofir.noy@outlook.com>
2020-03-19 19:58:03 +0200

    Fixed a strange quirk in LibGit2Sharp Read comment in code for more details

--- src/EngineParts/Storage.cs
+++ src/EngineParts/Storage.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.IO;
+using System.Linq;
 using LibGit2Sharp;
 
 namespace GitEngineDB.EngineParts
@@ -19,29 +20,41 @@ namespace GitEngineDB.EngineParts
         }
 
         public ConcurrentDictionary<string, string> CreateCache()
-        {   
+        {
+            var root = string.Empty;
             if (!Directory.Exists(_dbBasePath))
             {
                 Directory.CreateDirectory(_dbBasePath);
-                Repository.Init(_dbBasePath);
             }
             else
             {
-                var root = Repository.Discover(_dbBasePath);
-                if(string.IsNullOrEmpty(root))
+                root = Repository.Discover(_dbBasePath);
+            }
+
+            if(string.IsNullOrEmpty(root))
+            {
+                Repository.Init(_dbBasePath);
+                // This is a very strange quirk when calling Init
+                // A link file (_git_<random hex>) is created to a none existant testing folder
+                // I think that this will be fixed in future versions of LibGit2Sharp
+                // In mean time, since I expect an empty folder after the Init 
+                // I will just delete everything except .git folder
+                foreach (var dir in Directory.GetDirectories(_dbBasePath)
+                                             .Where(d => d != Path.Combine(_dbBasePath, ".git")))
                 {
-                    Repository.Init(_dbBasePath);
-                }
-                else if(root != Path.Combine(Path.GetFullPath(_dbBasePath), ".git\\"))
-                {
-                    throw new InvalidOperationException($"Directory {_dbBasePath} is a sub directory of an existing repository, unable to create a repository");
+                    Directory.Delete(dir);
                 }
             }
+
+            else if(root != Path.Combine(Path.GetFullPath(_dbBasePath), ".git\\"))
+            {
+                throw new InvalidOperationException($"Directory {_dbBasePath} is a sub directory of an existing repository, unable to create a repository");
+            }            
             
             var dbCache = new ConcurrentDictionary<string, string>();
             using (var repo = new Repository(_dbBasePath))
             {
-                var status = repo.RetrieveStatus(new StatusOptions() { IncludeUnaltered = true | false });
+                var status = repo.RetrieveStatus();                
                 if(status.IsDirty)
                 {
                     CommitChanges();
