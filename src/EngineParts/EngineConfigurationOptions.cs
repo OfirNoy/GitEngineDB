df96b2dfdf0069cf396bd991e2c5919ac4e27f43
Ofir Noy <ofir.noy@outlook.com>
2020-03-20 15:58:27 +0200

    0.0.9 Added options type, controls "write changes to disk" intervals and logger object reference

---
+++ src/EngineParts/EngineConfigurationOptions.cs
@@ -0,0 +1,10 @@
+﻿using Serilog;
+
+namespace GitEngineDB.EngineParts
+{
+    public class EngineConfigurationOptions
+    {
+        public ILogger Logger { get; set; }
+        public int WriteIntervals { get; set; } = 300;
+    }
+}
